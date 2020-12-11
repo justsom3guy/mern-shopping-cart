042978cef93db0d8bb14444bd10a2bc6e15f522a
Sidharth Nair <snair2103@gmail.com>
2020-12-11 10:30:28 +0530

    -add a navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">Hello world</div>;
+import React, { Component } from "react";
+
+import AppNavbar from "./components/appNavbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppNavbar />
+        hello world
+      </div>
+    );
+  }
 }
 
 export default App;
